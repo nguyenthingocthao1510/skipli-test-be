148fe50d017da3edc0ad8ee7bf22602f0759881c
ntnthao15102002 <ntnthao15102002@gmail.com>
2025-08-26 18:02:33 +0700

    fix: configure the key

--- config/firebase.js
+++ config/firebase.js
@@ -3,12 +3,14 @@ const dotenv = require("dotenv");
 
 dotenv.config();
 
+const privateKey = process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, "\n");
+
 admin.initializeApp({
   credential: admin.credential.cert({
     type: process.env.FIREBASE_TYPE,
     project_id: process.env.FIREBASE_PROJECT_ID,
     private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
-    private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"),
+    private_key: privateKey,
     client_email: process.env.FIREBASE_CLIENT_EMAIL,
     client_id: process.env.FIREBASE_CLIENT_ID,
     auth_uri: process.env.FIREBASE_AUTH_URI,
